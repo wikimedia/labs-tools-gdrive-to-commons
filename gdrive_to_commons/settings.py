2241fd37309a9e257dd036645b7ab24148f4af13
Tony Thomas <tony.thomas@keyflow.se>
2020-08-28 18:04:40 +0200

    Move static files and URL config to local_settings.py
    Change-Id: Ib0d5c10622aad99dae37600ea940b303deb959ac

--- gdrive_to_commons/settings.py
+++ gdrive_to_commons/settings.py
@@ -102,11 +102,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = "/google-drive-photos-to-commons/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "assets")]
 
 # All settings common to all environments
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
